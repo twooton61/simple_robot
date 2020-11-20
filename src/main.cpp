8bccbe474e75842b5c641a0e87a908b2b648fd9c
Terry Wooton <terry@oodle.io>
2020-11-20 09:25:05 -0500

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,34 @@
 /* servo */
 const uint8_t SERVO_PIN = 10;
 
-inline void reset_servo(Servo& servo) {
-  servo.write(90);
-}
+namespace servo_helper {
+  inline void reset(Servo& servo) {
+    servo.write(90);
+  }
 
-inline void wave_servo_left(Servo& servo) {
-  servo.write(0);
-}
+  inline void wave_left(Servo& servo) {
+    servo.write(0);
+  }
 
-inline void wave_servo_right(Servo& servo) {
-  servo.write(90);
+  inline void wave_right(Servo& servo) {
+    servo.write(90);
+  }
 }
 
 Servo servo;
 
 /* lcd */
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-inline void writeLCDDoubleLine(String line1, String line2) {
-  lcd.begin(16, 2);
-  lcd.print(line1);
-  lcd.setCursor(0, 1);
-  lcd.print(line2);
+
+namespace lcd_helper {
+  inline void writeLCDDoubleLine(LiquidCrystal& lcd, String line1, String line2) {
+    lcd.begin(16, 2);
+    lcd.print(line1);
+    lcd.setCursor(0, 1);
+    lcd.print(line2);
+  }
 }
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 /* setup ultrasonic sensor */
 const uint8_t ULTRASONIC_SENSOR_ECHO_DETECT_PIN = 8;
@@ -43,125 +49,129 @@ const uint8_t SENSOR_SEND_SIGNAL = 1;
 const unsigned int SENSOR_DISTANCE_IN_RANGE = 30;
 const unsigned int SENSOR_DISTANCE_WAY_FAR_AWAY = 1000;
 
-inline unsigned long get_cm_from_sensor() {
-  digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_STOP_SIGNAL);
-  delayMicroseconds(2);
-  digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_SEND_SIGNAL);
-  delayMicroseconds(10);
-  digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_STOP_SIGNAL);
+namespace ultrasonic_helper {
+  inline unsigned long get_cm_from_sensor() {
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_STOP_SIGNAL);
+    delayMicroseconds(2);
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_SEND_SIGNAL);
+    delayMicroseconds(10);
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, SENSOR_STOP_SIGNAL);
 
-  const unsigned long duration = pulseIn(ULTRASONIC_SENSOR_ECHO_DETECT_PIN, HIGH);
+    const unsigned long duration = pulseIn(ULTRASONIC_SENSOR_ECHO_DETECT_PIN, HIGH);
 
-  return int(duration / 2 / 29.412);
+    return int(duration / 2 / 29.412);
+  }
 }
 
 /* buzzer */
 const uint8_t PASSIVE_BUZZER_PIN = 6;
 const uint8_t BUZZER_ON = HIGH;
 const uint8_t BUZZER_OFF = LOW;
-namespace buzzer_note {
-  const int c = 261;
-  const int d = 294;
-  const int e = 329;
-  const int f = 349;
-  const int g = 391;
-  const int gS = 415;
-  const int a = 440;
-  const int aS = 455;
-  const int b = 466;
-  const int cH = 523;
-  const int cSH = 554;
-  const int dH = 587;
-  const int dSH = 622;
-  const int eH = 659;
-  const int fH = 698;
-  const int fSH = 740;
-  const int gH = 784;
-  const int gSH = 830;
-  const int aH = 880;
-}
-
-inline void play_buzzer_note(const int buzzer_note, const int duration)
-{
-  static int note_count = 0;
-
-  tone(PASSIVE_BUZZER_PIN, buzzer_note, duration);
-
-  if(note_count % 2 == 0)
-  {
-    wave_servo_left(servo);
-  }
-  else
-  {
-    wave_servo_right(servo);
+namespace buzzer_helper {
+  namespace note {
+    const int c = 261;
+    const int d = 294;
+    const int e = 329;
+    const int f = 349;
+    const int g = 391;
+    const int gS = 415;
+    const int a = 440;
+    const int aS = 455;
+    const int b = 466;
+    const int cH = 523;
+    const int cSH = 554;
+    const int dH = 587;
+    const int dSH = 622;
+    const int eH = 659;
+    const int fH = 698;
+    const int fSH = 740;
+    const int gH = 784;
+    const int gSH = 830;
+    const int aH = 880;
   }
 
-  delay(duration);
+  inline void play_note(const int buzzer_note, const int duration)
+  {
+    static int note_count = 0;
 
-  noTone(PASSIVE_BUZZER_PIN);
+    tone(PASSIVE_BUZZER_PIN, buzzer_note, duration);
 
-  delay(50);
+    if(note_count % 2 == 0)
+    {
+      servo_helper::wave_left(servo);
+    }
+    else
+    {
+      servo_helper::wave_right(servo);
+    }
 
-  note_count++;
-}
+    delay(duration);
 
-inline void play_star_wars() {
-  play_buzzer_note(buzzer_note::a, 500);
-  play_buzzer_note(buzzer_note::a, 500);
-  play_buzzer_note(buzzer_note::a, 500);
-  play_buzzer_note(buzzer_note::f, 350);
-  play_buzzer_note(buzzer_note::cH, 150);
-  play_buzzer_note(buzzer_note::a, 500);
-  play_buzzer_note(buzzer_note::f, 350);
-  play_buzzer_note(buzzer_note::cH, 150);
-  play_buzzer_note(buzzer_note::a, 650);
+    noTone(PASSIVE_BUZZER_PIN);
 
-  delay(500);
+    delay(50);
 
-  play_buzzer_note(buzzer_note::eH, 500);
-  play_buzzer_note(buzzer_note::eH, 500);
-  play_buzzer_note(buzzer_note::eH, 500);
-  play_buzzer_note(buzzer_note::fH, 350);
-  play_buzzer_note(buzzer_note::cH, 150);
-  play_buzzer_note(buzzer_note::gS, 500);
-  play_buzzer_note(buzzer_note::f, 350);
-  play_buzzer_note(buzzer_note::cH, 150);
-  play_buzzer_note(buzzer_note::a, 650);
+    note_count++;
+  }
 
-  delay(500);
+  inline void play_star_wars() {
+    play_note(buzzer_helper::note::a, 500);
+    play_note(buzzer_helper::note::a, 500);
+    play_note(buzzer_helper::note::a, 500);
+    play_note(buzzer_helper::note::f, 350);
+    play_note(buzzer_helper::note::cH, 150);
+    play_note(buzzer_helper::note::a, 500);
+    play_note(buzzer_helper::note::f, 350);
+    play_note(buzzer_helper::note::cH, 150);
+    play_note(buzzer_helper::note::a, 650);
 
-  play_buzzer_note(buzzer_note::aH, 500);
-  play_buzzer_note(buzzer_note::a, 300);
-  play_buzzer_note(buzzer_note::a, 150);
-  play_buzzer_note(buzzer_note::aH, 500);
-  play_buzzer_note(buzzer_note::gSH, 325);
-  play_buzzer_note(buzzer_note::gH, 175);
-  play_buzzer_note(buzzer_note::fSH, 125);
-  play_buzzer_note(buzzer_note::fH, 125);
-  play_buzzer_note(buzzer_note::fSH, 250);
+    delay(500);
 
-  delay(325);
+    play_note(buzzer_helper::note::eH, 500);
+    play_note(buzzer_helper::note::eH, 500);
+    play_note(buzzer_helper::note::eH, 500);
+    play_note(buzzer_helper::note::fH, 350);
+    play_note(buzzer_helper::note::cH, 150);
+    play_note(buzzer_helper::note::gS, 500);
+    play_note(buzzer_helper::note::f, 350);
+    play_note(buzzer_helper::note::cH, 150);
+    play_note(buzzer_helper::note::a, 650);
 
-  play_buzzer_note(buzzer_note::aS, 250);
-  play_buzzer_note(buzzer_note::dSH, 500);
-  play_buzzer_note(buzzer_note::dH, 325);
-  play_buzzer_note(buzzer_note::cSH, 175);
-  play_buzzer_note(buzzer_note::cH, 125);
-  play_buzzer_note(buzzer_note::b, 125);
-  play_buzzer_note(buzzer_note::cH, 250);
+    delay(500);
 
-  delay(350);
+    play_note(buzzer_helper::note::aH, 500);
+    play_note(buzzer_helper::note::a, 300);
+    play_note(buzzer_helper::note::a, 150);
+    play_note(buzzer_helper::note::aH, 500);
+    play_note(buzzer_helper::note::gSH, 325);
+    play_note(buzzer_helper::note::gH, 175);
+    play_note(buzzer_helper::note::fSH, 125);
+    play_note(buzzer_helper::note::fH, 125);
+    play_note(buzzer_helper::note::fSH, 250);
 
-  play_buzzer_note(buzzer_note::f, 250);
-  play_buzzer_note(buzzer_note::gS, 500);
-  play_buzzer_note(buzzer_note::f, 375);
-  play_buzzer_note(buzzer_note::cH, 125);
-  play_buzzer_note(buzzer_note::a, 500);
-  play_buzzer_note(buzzer_note::f, 375);
-  play_buzzer_note(buzzer_note::cH, 125);
-  play_buzzer_note(buzzer_note::a, 650);
+    delay(325);
 
-  delay(650);
+    play_note(buzzer_helper::note::aS, 250);
+    play_note(buzzer_helper::note::dSH, 500);
+    play_note(buzzer_helper::note::dH, 325);
+    play_note(buzzer_helper::note::cSH, 175);
+    play_note(buzzer_helper::note::cH, 125);
+    play_note(buzzer_helper::note::b, 125);
+    play_note(buzzer_helper::note::cH, 250);
+
+    delay(350);
+
+    play_note(buzzer_helper::note::f, 250);
+    play_note(buzzer_helper::note::gS, 500);
+    play_note(buzzer_helper::note::f, 375);
+    play_note(buzzer_helper::note::cH, 125);
+    play_note(buzzer_helper::note::a, 500);
+    play_note(buzzer_helper::note::f, 375);
+    play_note(buzzer_helper::note::cH, 125);
+    play_note(buzzer_helper::note::a, 650);
+
+    delay(650);
+  }
 }
 
 void setup()
@@ -176,14 +186,14 @@ void setup()
 
   servo.attach(SERVO_PIN);
 
-  reset_servo(servo);
+  servo_helper::reset(servo);
 }
 
 void loop()
 {
   Serial.println("loop");
 
-  const unsigned long cm_from_ultrasonic_sensor = get_cm_from_sensor();
+  const unsigned long cm_from_ultrasonic_sensor = ultrasonic_helper::get_cm_from_sensor();
   if (cm_from_ultrasonic_sensor <= SENSOR_DISTANCE_WAY_FAR_AWAY) {
     Serial.print("sensor: distance is ");
     Serial.print(cm_from_ultrasonic_sensor);
@@ -195,18 +205,18 @@ void loop()
     }
     else if (cm_from_ultrasonic_sensor <= SENSOR_DISTANCE_IN_RANGE)
     {
-      writeLCDDoubleLine("Motion Detected", String(String(cm_from_ultrasonic_sensor/2.54) + "in away"));
+      lcd_helper::writeLCDDoubleLine(lcd, "Motion Detected", String(String(cm_from_ultrasonic_sensor/2.54) + "in away"));
 
       delay(3000);
 
-      writeLCDDoubleLine("Playing", "Star Wars");
+      lcd_helper::writeLCDDoubleLine(lcd, "Playing", "Star Wars");
 
-      play_star_wars();
+      buzzer_helper::play_star_wars();
 
     } else {
-      writeLCDDoubleLine("Hello! Wave", "for a song");
+      lcd_helper::writeLCDDoubleLine(lcd, "Hello! Wave", "for a song");
 
-      reset_servo(servo);
+      servo_helper::reset(servo);
     }
   }
 
